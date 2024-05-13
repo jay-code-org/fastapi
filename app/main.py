32b5e19f9d3f01a4b0199b0495b78015248e8da3
Makame Juma <makame.juma@forest-interactive.com>
2024-05-13 12:57:17 +0800

    added sync endpoint

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from .database import engine
-from . import models
-from .routers import user, todo, auth, admin
+from sqlalchemy.orm import Session
+from . import models, database, schemas, utils
+from .routers import user, todo, auth
 
 
 # This line makes sure all models are applied in the database before running the app
-models.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 
@@ -20,9 +20,41 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(todo.router)
-app.include_router(admin.router)
 
 
-@app.get("/test")
-def root():
-    return {"message": "Success"}
+@app.get("/sync")
+def root(db: Session = Depends(database.get_db)):
+    check_user = db.query(models.User).filter(
+        models.User.email == 'admin@fast.com').first()
+
+    if check_user is None:
+
+        user_status = db.query(models.Status).filter(
+            models.Status.name == schemas.StatusEnum.Active.value).first()
+
+        if user_status is None:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Something went wrong")
+
+        user_role = db.query(models.Role).filter(
+            models.Role.name == schemas.RoleEnum.Admin.value).first()
+
+        if user_role is None:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Something went wrong")
+
+        new_user = {"email": "", "full_name": "", "phone": ""}
+        new_user.update({"password": utils.hash_password(
+            "1234"), "status_id": user_status.id, "role_id": user_role.id})
+
+        user = models.User(**new_user)
+
+        try:
+            db.add(user)
+            db.commit()
+            db.refresh(user)
+        except Exception as e:
+            db.rollback()
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=f"Unable to perform specified operation")
+    return {"message": "Synchronized successfully!"}
